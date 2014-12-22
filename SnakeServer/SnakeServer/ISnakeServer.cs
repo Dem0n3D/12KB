308e66e00ef1bef56a748c3a9d1b2674a8c0e776
Sergei <S.Sergei.Ur@gmail.com>
2014-12-22 16:54:57 +0500

    Добавил SnakeServer

---
+++ SnakeServer/SnakeServer/ISnakeServer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace SnakeServer {
+	[ServiceContract]
+	public interface ISnakeServer {
+		[OperationContract]
+		void DoWork();
+
+		[OperationContract]
+		void Start(ref Snake snake, ref Field field, int height, int widht);
+
+		[OperationContract]
+		void Motion(ref Snake snake, ref Field field);
+
+		[OperationContract]
+		void AddBonus(ref Snake snake, ref Field field);
+	}
+}
---
+++ SnakeServer/SnakeServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace SnakeServer {
+	internal class Program {
+		private static void Main() {
+			ServiceHost serviceHost = new ServiceHost(typeof(SnakeServer));
+			//serviceHost.AddServiceEndpoint(typeof(ISnakeServer), new NetTcpBinding(), "net.tcp://localhast:8875/");
+			serviceHost.Open();
+			Console.ReadKey();
+			serviceHost.Close();
+		}
+	}
+}
