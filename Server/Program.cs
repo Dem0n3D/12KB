3678110c7581f4e1120803526b960e2c1078b8b9
MaSkim94 <M5328@live.ru>
2014-12-22 14:06:06 +0500

    Блин не то кинул =(

---
+++ Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServiceHost serviceHost = new ServiceHost(typeof(Service));
+			serviceHost.AddServiceEndpoint(typeof(IService), new NetTcpBinding(), "net.tcp://localhast:8875/");
+			serviceHost.Open();
+			Console.WriteLine("Service is running...");
+			Console.WriteLine("Press any key to stop");
+			Console.ReadKey();
+			serviceHost.Close();
+			Console.WriteLine("Service was stoped");
+		}
+	}
+}
