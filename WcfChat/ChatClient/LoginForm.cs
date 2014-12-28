f8e0e68779eb82192a22e1404faf6fa4bf34ede7
Ekaterina_Chernova <katya94fm@mail.ru>
2014-12-29 02:57:23 +0500

    Add ChatClient, Login Form

---
+++ WcfChat/ChatClient/LoginForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        public string UserName { get; set; }
+
+
+        private void txtUserName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                
+            }
+
+
+        }
+    }
+}
