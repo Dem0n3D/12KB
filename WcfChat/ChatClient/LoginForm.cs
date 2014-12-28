019d37ee0995aecdcfb11c332e8130db0aa08c5b
Ekaterina_Chernova <katya94fm@mail.ru>
2014-12-29 02:59:01 +0500

    ChatClient, add button Login

--- WcfChat/ChatClient/LoginForm.cs
+++ WcfChat/ChatClient/LoginForm.cs
@@ -18,12 +18,26 @@ namespace ChatClient
 
         public string UserName { get; set; }
 
+	private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtUserName.Text))
+            {
+                this.UserName = txtUserName.Text;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please insert a username", "Error validation",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+                
+        }
 
         private void txtUserName_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == (char)13)
             {
-                
+                btnOK_Click(sender, e);
             }
 
 
